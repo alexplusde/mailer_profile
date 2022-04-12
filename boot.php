a5df68d4fb4389172706af4a8de7c105d5454b55
Alexander Walther <aw@alexplus.de>
2022-04-12 09:04:22 +0200

    check for phpmailer addon in boot.php

--- boot.php
+++ boot.php
@@ -1,5 +1,5 @@
 <?php
-if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+if (rex_addon::get('yform')->isAvailable() && rex_addon::get('phpmailer')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_dataset::setModelClass(
         'rex_mailer_profile',
         mailer_profile::class
