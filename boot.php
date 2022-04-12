619f33208c51dd6f4abe40a7208481f6bc51cc8b
Alexander Walther <aw@alexplus.de>
2022-04-12 09:03:35 +0200

    register ep

--- boot.php
+++ boot.php
@@ -4,4 +4,6 @@ if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
         'rex_mailer_profile',
         mailer_profile::class
     );
+
+    rex_extension::register('PHPMAILER_CONFIG', ['mailer_profile','epMailerProfile']);
 }
