1b89c1e9decd1ee6428db8024e50a3f01dc4ccc0
Alexander Walther <aw@alexplus.de>
2022-04-20 12:32:31 +0200

    donation notice, version bump

---
+++ pages/phpmailer.settings.php
@@ -0,0 +1,18 @@
+
+<div class="row">
+    <div class="col-lg-8">
+
+</div>
+    <div class="col-lg-4">
+        <?php
+
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=mailer_profile"><img src="'.rex_url::addonAssets('mailer_profile', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'info', false);
+$fragment->setVar('title', $this->i18n('mailer_profile_donate'), false);
+$fragment->setVar('body', '<p>' . $this->i18n('mailer_profile_info_donate') . '</p>' . $anchor, false);
+echo !rex_config::get("alexplusde", "donated") ? $fragment->parse('core/page/section.php') : "";
+?>
+    </div>
+</div>
