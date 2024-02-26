1a37f0a21077e645d5f7587fa75373f27e2cecf4
Alexander Walther <aw@alexplus.de>
2024-02-26 17:47:32 +0100

    Update uninstall.php
    Verlorengegangenen Code wieder einbauen.

--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,6 @@
 <?php
 
 rex_config::removeNamespace('mailer_profile');
+if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+    rex_yform_manager_table_api::removeTable('rex_mailer_profile');
+}
