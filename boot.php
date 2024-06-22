7d1654a24c857aa5c1150308d52495a7513135af
alxndr-w <alxndr-w@users.noreply.github.com>
2024-06-22 10:03:40 +0000

    Apply php-cs-fixer changes

--- boot.php
+++ boot.php
@@ -9,9 +9,9 @@ if (rex_addon::get('yform')->isAvailable() && rex_addon::get('phpmailer')->isAva
 
 // Listendarstellung in YForm verbessern
 
-rex_extension::register('YFORM_DATA_LIST', function ($ep) {
-    if ($ep->getParam('table')->getTableName()=="rex_mailer_profile") {
-        // @var rex_list $list
+rex_extension::register('YFORM_DATA_LIST', static function ($ep) {
+    if ('rex_mailer_profile' == $ep->getParam('table')->getTableName()) {
+        /** @var rex_list $list */
         $list = $ep->getSubject();
 
         // Reihenfolge verändern: Zuerst `mailer_type`
@@ -19,28 +19,27 @@ rex_extension::register('YFORM_DATA_LIST', function ($ep) {
         $list->setColumnFormat(
             'mailer',
             'custom',
-            function ($a) {
+            static function ($a) {
                 // Gebe den Wert des Mailertyps als Bootstrap-Badge aus, darunter die Werte zu Authentifizierung und Benutzername
                 $mailer = $a['list']->getValue('mailer');
                 $return = '<div class="text-nowrap">';
-                if ($mailer == 'smtp') {
+                if ('smtp' == $mailer) {
                     $return .= '<table class="table table-sm table-borderless text-nowrap">';
                     if ($a['list']->getValue('smtp_debug') > 0) {
                         $return .= '<tr><th>Debugging:</th><td><i class="fas fa-exclamation-triangle text-danger"></i> aktiviert</td></tr>';
                     }
-                    $return .= '<tr><th>Host:</th><td>'. $a['list']->getValue('host') . ':'. $a['list']->getValue('port'). '</td></tr>';
-                    $return .= '<tr><th>SMTPSecure:</th><td>'. $a['list']->getValue('smtpsecure'). '</td></tr>';;
-                    $return .= '<tr><th>SMTPAuth:</th><td>'. $a['list']->getValue('smtpauth'). '</td></tr>';;
-                    $return .= '<tr><th>SMTPAutoTLS:</th><td>'. $a['list']->getValue('smtpautotls'). '</td></tr>';;
-                    $return .= '<tr><th>Username:</th><td>'. $a['list']->getValue('username'). '</td></tr>';;
+                    $return .= '<tr><th>Host:</th><td>' . $a['list']->getValue('host') . ':' . $a['list']->getValue('port') . '</td></tr>';
+                    $return .= '<tr><th>SMTPSecure:</th><td>' . $a['list']->getValue('smtpsecure') . '</td></tr>';
+                    $return .= '<tr><th>SMTPAuth:</th><td>' . $a['list']->getValue('smtpauth') . '</td></tr>';
+                    $return .= '<tr><th>SMTPAutoTLS:</th><td>' . $a['list']->getValue('smtpautotls') . '</td></tr>';
+                    $return .= '<tr><th>Username:</th><td>' . $a['list']->getValue('username') . '</td></tr>';
                     $return .= '</table>';
-    
                 } else {
-                    $return .= '<span class="badge badge-primary">'. $mailer .'</span>';
+                    $return .= '<span class="badge badge-primary">' . $mailer . '</span>';
                 }
                 $return .= '</div>';
                 return $return;
-            }
+            },
         );
 
         // Überflüssige Columns entfernen
@@ -56,16 +55,16 @@ rex_extension::register('YFORM_DATA_LIST', function ($ep) {
         $list->setColumnFormat(
             'from',
             'custom',
-            function ($a) {
+            static function ($a) {
                 // fromname, form und Lesebestätigungsoption sowie bcc in einer Tabelle ausgeben
                 $return = '<table class="table table-sm table-borderless text-nowrap">';
-                $return .= '<tr><th>Name:</th><td>'. $a['list']->getValue('fromname').'</td></tr>';
-                $return .= '<tr><th>E-Mail:</th><td>'. $a['list']->getValue('from') .'</td></tr>';
-                $return .= '<tr><th>Lesebestätigung:</th><td>'. ($a['list']->getValue('confirmto') ? '<i class="fas fa-times text-danger"></i> '. $a['list']->getValue('confirmto') : '') .'</td></tr>';
-                $return .= '<tr><th>BCC:</th><td>'. ($a['list']->getValue('bcc') ? '<i class="fas fa-times text-danger"></i> '. $a['list']->getValue('bcc') : '') .'</td></tr>';
+                $return .= '<tr><th>Name:</th><td>' . $a['list']->getValue('fromname') . '</td></tr>';
+                $return .= '<tr><th>E-Mail:</th><td>' . $a['list']->getValue('from') . '</td></tr>';
+                $return .= '<tr><th>Lesebestätigung:</th><td>' . ($a['list']->getValue('confirmto') ? '<i class="fas fa-times text-danger"></i> ' . $a['list']->getValue('confirmto') : '') . '</td></tr>';
+                $return .= '<tr><th>BCC:</th><td>' . ($a['list']->getValue('bcc') ? '<i class="fas fa-times text-danger"></i> ' . $a['list']->getValue('bcc') : '') . '</td></tr>';
                 $return .= '</table>';
                 return $return;
-            }
+            },
         );
         $list->removeColumn('fromname');
         $list->removeColumn('confirmto');
@@ -75,18 +74,20 @@ rex_extension::register('YFORM_DATA_LIST', function ($ep) {
         $list->setColumnFormat(
             'header',
             'custom',
-            function ($a) {
+            static function ($a) {
                 // Serialisierten Header umwandeln
                 $headers = json_decode($a['list']->getValue('header'), true);
-                if(!is_array($headers)) return '';
+                if (!is_array($headers)) {
+                    return '';
+                }
 
                 $return = '<table class="table table-sm table-borderless text-nowrap">';
                 foreach ($headers as $header) {
-                    $return .= '<tr><th>'. $header[0] .'</th><td>'. $header[1] .'</td></tr>';
+                    $return .= '<tr><th>' . $header[0] . '</th><td>' . $header[1] . '</td></tr>';
                 }
                 $return .= '</table>';
                 return $return;
-            }
+            },
         );
 
         // Zusätzliche Einstellungen des Profils ausgeben, z.B. E-Mail-Log und E-Mail-Archivierung aktiviert
@@ -96,17 +97,16 @@ rex_extension::register('YFORM_DATA_LIST', function ($ep) {
         $list->setColumnFormat(
             'settings',
             'custom',
-            function ($a) {
+            static function ($a) {
                 $return = '<table class="table table-sm table-borderless text-nowrap">';
-                $return .= '<tr><th>Log:</th><td>'. ($a['list']->getValue('logging') ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') .'</td></tr>';
-                $return .= '<tr><th>Archivierung:</th><td>'. ($a['list']->getValue('archive') ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') .'</td></tr>';
+                $return .= '<tr><th>Log:</th><td>' . ($a['list']->getValue('logging') ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') . '</td></tr>';
+                $return .= '<tr><th>Archivierung:</th><td>' . ($a['list']->getValue('archive') ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') . '</td></tr>';
                 $return .= '</table>';
                 return $return;
-            }
+            },
         );
 
         $list->removeColumn('logging');
         $list->removeColumn('archive');
-
     }
 });
